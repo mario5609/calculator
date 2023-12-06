c01a0b90c11150c71cce28a28c0a830266b61fa8
mario5609 <mariocmanuel@gmail.com>
2023-12-06 13:01:44 +0530

    calculator

---
+++ cal.js
@@ -0,0 +1,86 @@
+function ad9(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n9").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad8(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n8").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad7(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n7").value;
+    document.getElementById("bx").value=(x+a);
+    console.log(a);
+}
+function ad6(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n6").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad5(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n5").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad4(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n4").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad3(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n3").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad2(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n2").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad1(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n1").value;
+    document.getElementById("bx").value=(x+a);
+}
+function ad0(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("n0").value;
+    document.getElementById("bx").value=(x+a);
+}
+
+function adp(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("s+").value;
+    document.getElementById("bx").value=(x+a);
+}
+function admi(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("s-").value;
+    document.getElementById("bx").value=(x+a);
+}
+function adm(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("s*").value;
+    document.getElementById("bx").value=(x+a);
+}
+function addi(){
+    let x= document.getElementById("bx").value;
+    let a= document.getElementById("s/").value;
+    document.getElementById("bx").value=(x+a);
+}
+
+
+function ent(){
+    let a= document.getElementById("bx").value;
+    let y= eval(a);
+    document.getElementById("bx").value=(y);
+}
+function cr(){
+    document.getElementById("bx").value= " ";
+}
+function br(){
+    let a= document.getElementById("bx").value;
+    document.getElementById("bx").value= a.slice(0,-1);
+}
